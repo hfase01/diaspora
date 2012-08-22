5b5c0f13c36006bef5370edafbb4fae14bd5f5f7
Hans Fase <hfase01@gmail.com>
2012-08-21 23:28:15 -0400

    Added resque pid to start up script and more process to start.

--- resque.sh
+++ resque.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-   RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
-&& RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
-&& RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
-&& RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
-&& RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid
+    RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
+ && RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
+ && RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
+ && RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
+ && RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
+ && RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
+ && RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
+ && RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
+ && RAILS_ENV=production QUEUE=* RAILS_ENV=production BACKGROUND=yes bundle exec rake resque:work PIDFILE=/home/diaspora/diaspora-hfase/tmp/pids/resque.pid \
+exit 0
